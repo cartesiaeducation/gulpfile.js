b16bbe0f01a6a07613f026709914b53476249095
Andréa Maugars <andrea.maugars@gmail.com>
2017-06-27 14:44:01 +0200

    Add spread operator syntax to objects

--- webpack.dev.js
+++ webpack.dev.js
@@ -9,6 +9,7 @@ module.exports = {
                 loader: 'babel-loader',
                 options: {
                     presets: ['es2015', 'react'],
+                    plugins: ["transform-object-rest-spread"],
                     cacheDirectory: true
                 }
             }
