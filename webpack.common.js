7a035b4dcb0e998f0c2047c1fc9fef658469b707
Ahmed <khalil1608@gmail.com>
2017-12-26 18:24:15 +0100

    Fix symlink not working with composer-dev

--- webpack.common.js
+++ webpack.common.js
@@ -32,6 +32,9 @@ module.exports = {
                 use: "imports-loader?jQuery=>window.jQuery,$=>window.jQuery,define=>false,require=>false,exports=>false,this=>window"
             }
         ]
+    },
+    resolve: {
+        symlinks: false
     }
 };
 
